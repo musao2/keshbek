1f9b2be1ae4607b77f34bd05fe8121700202238d
Muhammadsodiq Isomov <isomovmuhammadsodiq58@gmail.com>
2026-09-09 13:59:10 +0500

    Yangilanish kiritildi QR togirlashga xarakat qilindi

--- keshbek/vite.config.js
+++ keshbek/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api-keshbek.72-62-0-138.sslip.io',
+        changeOrigin: true,
+      }
+    }
+  }
 })
